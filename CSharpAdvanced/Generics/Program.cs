7409ea5243f72b43ad1f5debd89d6b5325a89d3b
HeptaDecane <sooraj1999vs@gmail.com>
2022-07-24 14:54:21 +0530

    generics

---
+++ CSharpAdvanced/Generics/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Generics {
+    
+    class GenericList<T> {
+        public void Add(T obj) {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index] {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    class GenericDictionary<TKey, TValue> {
+        public void Add(TKey key, TValue value) {
+            throw new NotImplementedException();
+        }
+        
+        public void Get(TKey key) {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Utils<T> where T:IComparable {
+        public static T Max(T a, T b){
+           return a.CompareTo(b)>0 ? a:b;
+        }
+    }
+    
+    class Book {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+    }
+    
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new GenericList<int>();
+            int num = 10;
+            numbers.Add(num);
+
+            var books = new GenericList<Book>();
+            Book book = new Book { Isbn = "1707", Title = "C# Advanced" };
+            books.Add(book);
+        }
+    }
+}
