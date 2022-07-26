4a96f41122b5b931e019db5ae5673d8db10fa82d
HeptaDecane <sooraj1999vs@gmail.com>
2022-07-27 00:15:49 +0530

    remove lambda exps

--- WorkingWithData/WorkingWithData/Services/BooksService.cs
+++ WorkingWithData/WorkingWithData/Services/BooksService.cs
@@ -13,13 +13,23 @@ public class BooksService {
         _collection = mongoDatabase.GetCollection<Book>("Books");
     }
 
-    public async Task<List<Book>> GetAsync() => await _collection.Find(_ => true).ToListAsync();
+    public async Task<List<Book>> GetAsync() {
+        return await _collection.Find(x => true).ToListAsync();
+    }
 
-    public async Task<Book?> GetAsync(int id) => await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+    public async Task<Book?> GetAsync(int id) {
+        return await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+    }
 
-    public async Task CreateAsync(Book newBook) => await _collection.InsertOneAsync(newBook);
+    public async Task CreateAsync(Book newBook) { 
+        await _collection.InsertOneAsync(newBook);
+    }
 
-    public async Task UpdateAsync(int id, Book updatedBook) => await _collection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+    public async Task UpdateAsync(int id, Book updatedBook) {
+        await _collection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+    }
 
-    public async Task DeleteAsync(int id) => await _collection.DeleteOneAsync(x => x.Id == id);
+    public async Task DeleteAsync(int id) {
+        await _collection.DeleteOneAsync(x => x.Id == id);
+    }
 }
