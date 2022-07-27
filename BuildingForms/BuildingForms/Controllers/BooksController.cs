2f486def394e248e33626cb41d17aa01d1066fac
Sooraj VS <sooraj.vs@bajajfinserv.in>
2022-07-27 15:11:00 +0530

    binding html form

---
+++ BuildingForms/BuildingForms/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using BuildingForms.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingForms.Controllers;
+
+public class BooksController : Controller
+{
+    // GET
+    public IActionResult Index() {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Book book)
+    {
+        Console.WriteLine(book);
+        return View(book);
+    }
+}
---
+++ BuildingForms/BuildingForms/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace BuildingForms.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Category { get; set; }
+    public int Stock { get; set; }
+
+    public override string ToString()
+    {
+        return $"Book:\n" +
+               $"\tId: {Id}\n" +
+               $"\tTitle: {Title}\n" +
+               $"\tAuthor: {Author}\n" +
+               $"\tCategory: {Category}\n" +
+               $"\tStock: {Stock}";
+    }
+}
