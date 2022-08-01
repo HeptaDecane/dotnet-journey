cb9261ade024a03c098fb061f0f6aec17f32b84c
HeptaDecane <sooraj1999vs@gmail.com>
2022-08-01 12:30:44 +0530

    .net Web API for todo items

---
+++ WebApis/WebApis/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApis.Controllers;
+
+[ApiController]
+[Route("")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public ActionResult Get()
+    {
+        return Ok("Api Running...");
+    }
+}
---
+++ WebApis/WebApis/DatabaseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApis;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+}
