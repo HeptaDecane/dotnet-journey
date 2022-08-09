76a1675f144527252ca4206181efc940542392dd
HeptaDecane <sooraj1999vs@gmail.com>
2022-08-09 10:49:59 +0530

    add anti-forgery validation

--- BuildingForms/BuildingForms/Controllers/CustomersController.cs
+++ BuildingForms/BuildingForms/Controllers/CustomersController.cs
@@ -16,6 +16,7 @@ public class CustomersController : Controller {
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public ActionResult Create(Customer customer)
     {
         Console.WriteLine(customer);
