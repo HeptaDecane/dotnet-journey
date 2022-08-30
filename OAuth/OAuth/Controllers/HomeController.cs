dcc1a511c6ca6bf24cf56e62fc37b6a78df88e6b
HeptaDecane <sooraj1999vs@gmail.com>
2022-08-30 09:52:18 +0530

    Google Auth

---
+++ OAuth/OAuth/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.Extensions.Logging;
+using OAuth.Models;
+
+namespace OAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var claims = User.Claims.Select(claim => new {
+                claim.Issuer,
+                claim.OriginalIssuer,
+                claim.Type,
+                claim.Value
+            });
+            return Ok(claims);
+        }
+
+        [Route("login")]
+        public IActionResult Login()
+        {
+            var properties = new AuthenticationProperties {
+                RedirectUri = Url.Action("Index"),
+            };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [Route("login-response")]
+        public async Task<IActionResult> LoginResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync("GoogleOAuth2.0");
+ 
+            var claims = result.Principal.Claims.Select(claim => new{
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+            return Ok(claims);
+        }
+
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("GoogleOAuth2.0");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
