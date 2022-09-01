ee1a3c181e50490ef8bf80430d009dfd0d969fed
HeptaDecane <sooraj1999vs@gmail.com>
2022-09-01 17:52:38 +0530

    session and state management

---
+++ Sessions/Sessions/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sessions.Models;
+
+namespace Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private string Pad(string text)
+        {
+            var padding = 3 - ((text.Length + 3) % 4);
+            if (padding == 0)
+            {
+                return text;
+            }
+            return text + new string('=', padding);
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var sessionId = HttpContext.Session.Id;
+            var user = HttpContext.Session.GetString("user");
+            
+            if(string.IsNullOrEmpty(user))
+                return RedirectToAction("Login", "Home");
+            
+            ViewData["sessionId"] = sessionId;
+            ViewData["user"] = user;
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Auth auth)
+        {
+            if(!ModelState.IsValid)
+                return View();
+
+            if (auth.Username == "testuser" && auth.Password == "testpass")
+            {
+                HttpContext.Session.SetString("user","testuser");
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sessions/Sessions/Models/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sessions.Models
+{
+    public class Auth
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
